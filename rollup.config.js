c3bfa1b042987dec68ce011dba80382ea681894c
MDaemon Technologies, Ltd. <jonathan.ehman@mdaemon.com>
2024-08-20 10:18:30 -0500

    Corrected the name of the class for the UMD file
    The umd file was using processQueue, when it should have been ProcessQueue

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,7 @@ export default [
   {
     input: "src/processQueue.ts",
     output: [
-      {
-        file: "dist/processQueue.umd.js", format: "umd", name: "processQueue", exports: "default" },
+      { file: "dist/processQueue.umd.js", format: "umd", name: "ProcessQueue", exports: "default" },
       { file: "dist/processQueue.cjs", format: "cjs", exports: "default" },
       { file: "dist/processQueue.mjs", format: "es" }
     ],
