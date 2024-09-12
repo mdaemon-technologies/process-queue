5aae5fab0ed84128552a73655fc71cad6d07f186
MDaemon Technologies, Ltd. <jonathan.ehman@mdaemon.com>
2024-09-12 12:22:39 -0500

    Minified

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import typescript from "@rollup/plugin-typescript"
+import terser from '@rollup/plugin-terser';
 
 export default [
   {
     input: "src/processQueue.ts",
     output: [
       { file: "dist/processQueue.umd.js", format: "umd", name: "ProcessQueue", exports: "default" },
-      { file: "dist/processQueue.cjs", format: "cjs", exports: "default" },
-      { file: "dist/processQueue.mjs", format: "es" }
+      { file: "dist/processQueue.cjs", format: "cjs", name: "ProcessQueue", exports: "default" },
+      { file: "dist/processQueue.mjs", format: "es", name: "ProcessQueue" }
     ],
     plugins: [
-      typescript()
+      typescript(),
+      terser()
     ]
   }
 ]
